341870e52d02fb9b2fd71da1ab4c0f753fe36162
okusama <david.naud75@live.fr>
2017-07-10 21:29:27 +0200

    test sql

--- index.php
+++ index.php
@@ -3,3 +3,11 @@
 
 $main = "ressources/template/contact.html";
 include "ressources/view/layout.phtml";
+
+include_once "src/request.php";
+
+if (isset($_POST["send"])){
+
+        saveMessage($db, $_POST["name"], $_POST["mail"], $_POST["object"], $_POST["message"]);
+
+}
--- src/request.php
+++ src/request.php
@@ -1,9 +1,27 @@
 <?php
 
-function connectionToDataBase(){
+include_once "config/connectDB.php";
 
+function saveMessage($db, $name, $mail, $object, $content){
 
-        $connection = new PDO ($db, $user, $password);
+        $sources =
+                " INSERT INTO `messages`(`name`, `mail`, `object`, `content`)
+                VALUES (?, ?, ?, ?)";
 
+        $query = $db->prepare($sources);
+        $query->execute([$name, $mail, $object, $content]);
+
+}
+
+function getMessage($db){
+
+        $sources =
+                " SELECT `date`, `name`, `mail`, `object`, `content`
+                FROM `messages`";
+
+        $query = $db->prepare($sources);
+        $query->execute();
+
+        $display = $query->fetchAll(PDO::FETCH_ASSOC);
 
 }
