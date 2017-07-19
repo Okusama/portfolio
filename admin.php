a03bc2044be0f9798583a7d4ab0cc38f379c1de7
okusama <david.naud75@live.fr>
2017-07-19 07:14:00 +0200

    *

--- admin.php
+++ admin.php
@@ -3,8 +3,13 @@
 $main = "ressources/template/admin/admin.phtml";
 include_once "ressources/view/layout.phtml";
 
-if ($_FILES['icone']['error'] > 0){
+$uploadDirection = "/upload/realisation";
 
-	echo "Erreur lors du transfert";
+
+if ($_FILES["urlImage"]["error"] == UPLOAD_ERR_OK) {
+
+        $tmp_name = $_FILES["urlImage"]["tmp_name"];
+        $name = $_FILES["urlImage"]["name"];
+        move_uploaded_file($tmp_name, "$uploadDirection/$name");
 
 }
