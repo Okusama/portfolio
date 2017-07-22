98956121727eef29250f2ae3039e7e146a1f8055
okusama <david.naud75@live.fr>
2017-07-23 00:59:49 +0200

    upload

--- admin.php
+++ admin.php
@@ -3,13 +3,13 @@
 $main = "ressources/template/admin/admin.phtml";
 include_once "ressources/view/layout.phtml";
 
-$uploadDirection = "/upload/realisation";
+$uploadDirection = "ressources/upload/realisation";
 
 
 if ($_FILES["urlImage"]["error"] == UPLOAD_ERR_OK) {
 
         $tmp_name = $_FILES["urlImage"]["tmp_name"];
         $name = $_FILES["urlImage"]["name"];
-        move_uploaded_file($tmp_name, "$uploadDirection/$name");
+        move_uploaded_file($tmp_name, $uploadDirection."/".$name);
 
 }
