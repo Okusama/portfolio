d048593e9092e7ac4108f1c928c68ddc2e8324cd
okusama <david.naud75@live.fr>
2017-07-26 10:07:31 +0200

    modif admin

--- admin.php
+++ admin.php
@@ -1,15 +1,19 @@
 <?php
 
 $main = "ressources/template/admin/admin.phtml";
-include_once "ressources/view/layout.phtml";
+include_once "ressources/view/layoutadmin.phtml";
 
 $uploadDirection = "ressources/upload/realisation";
 
 
-if ($_FILES["urlImage"]["error"] == UPLOAD_ERR_OK) {
+if (isset($_FILES["urlImage"])) {
 
-        $tmp_name = $_FILES["urlImage"]["tmp_name"];
-        $name = $_FILES["urlImage"]["name"];
-        move_uploaded_file($tmp_name, $uploadDirection."/".$name);
+        if ($_FILES["urlImage"]["error"] == UPLOAD_ERR_OK) {
+
+                $tmp_name = $_FILES["urlImage"]["tmp_name"];
+                $name = $_FILES["urlImage"]["name"];
+                move_uploaded_file($tmp_name, $uploadDirection."/".$name);
+
+        }
 
 }
